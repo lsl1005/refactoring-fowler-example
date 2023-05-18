f1a3661769716a5f6fbe85f5ad794c5712c0b17e
lsl1005 <lsl1005@alu.ubu.es>
2023-05-18 16:40:01 +0100

    #2 Mover método a Rental

--- src/ubu/gii/dass/refactoring/Customer.java
+++ src/ubu/gii/dass/refactoring/Customer.java
@@ -39,7 +39,7 @@ public class Customer {
 			double thisAmount = 0;
 			Rental each = rentals.next();
 			// determine amounts for each line
-			thisAmount = amountFor(each);			
+			thisAmount = each.amountFor();			
 			// add frequent renter points
 			frequentRenterPoints++;
 			// add bonus for a two day new release rental
@@ -56,25 +56,5 @@ public class Customer {
 		result += "You earned " + String.valueOf(frequentRenterPoints)
 				+ " frequent renter points";
 		return result;
-	}
-
-	private double amountFor(Rental aRental) {
-		double thisAmount=0;
-		switch (aRental.getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			thisAmount += 2;
-			if (aRental.getDaysRented() > 2)
-				thisAmount += (aRental.getDaysRented() - 2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			thisAmount += aRental.getDaysRented() * 3;
-			break;
-		case Movie.CHILDRENS:
-			thisAmount += 1.5;
-			if (aRental.getDaysRented() > 3)
-				thisAmount += (aRental.getDaysRented() - 3) * 1.5;
-			break;
-		}
-		return thisAmount;
 	}
 }
